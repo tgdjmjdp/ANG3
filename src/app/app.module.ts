20a164c90bacce0743a0f130d596e09f5809d3c0
tgdjmjdp <tgdjmjdp2@gmail.com>
2019-08-07 02:08:01 +0700

    doing fine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material';
+import { MatButtonModule, MatExpansionModule, MatInputModule, MatCardModule, MatToolbarModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post/post-list/post-list.component';
 import { PostCreateComponent } from './post/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    MatCardModule,
     MatInputModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatToolbarModule,
     BrowserAnimationsModule
   ],
   providers: [],
